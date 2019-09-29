880c1931303c8f6db2facb110ed02f655fc21a05
andy <andy.prost82@gmail.com>
2019-09-29 17:51:52 -0500

    Add project files.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = null;
+            book.GradeAdded += OnGradeAdded;
+
+            book.AddGrade(90.5);
+            EnterGrades(null);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"for the book named: {book.Name}");
+            Console.WriteLine($"the lowest grade is {stats.Low}");
+            Console.WriteLine($"the highest grade is {stats.High}");
+            Console.WriteLine($"the average grade is {stats.Average:N1}");
+            Console.WriteLine($"the letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+                if (input.Contains("q"))
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                    // ..
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
+        }
+    }
+}
---
+++ src/GradeBook/Statistics.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    public class Statistics
+    {
+        public double High, Low, Sum;
+        public int Count;
+
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
+
+        public char Letter
+        {
+            get
+            {
+                switch (Average)
+                {
+                    case var d when d > 90.0:
+                        return 'A';
+                    case var d when d > 80.0:
+                        return 'B';
+                    case var d when d > 70.0:
+                        return 'C';
+                    case var d when d > 60.0:
+                        return 'D';
+                    default:
+                        return 'F';
+                }
+            }
+        }
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count += 1;
+            High= Math.Max(number, High);
+            Low = Math.Min(number, Low);
+        }
+
+        public Statistics()
+        {
+            Count = 0;
+            Sum = 0.0;
+            High = double.MinValue;
+            Low = double.MaxValue;
+        }
+    }
+}
